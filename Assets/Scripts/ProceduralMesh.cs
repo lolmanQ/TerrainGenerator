1b7009a520fcff0175d2b17005b2be72cba4e024
Elias <32110106+lolmanQ@users.noreply.github.com>
2021-10-29 12:04:49 +0200

    quad line added

--- Assets/Scripts/ProceduralMesh.cs
+++ Assets/Scripts/ProceduralMesh.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class ProceduralMesh : MonoBehaviour
 {
+	public static List<Vector3> VertexList = new List<Vector3>();
+	public static Dictionary<Vector3, int> VertexPointer = new Dictionary<Vector3, int>();
+
 	public bool autoUpdate = false;
 
 	Mesh mesh;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-		
+	// Start is called before the first frame update
+	void Start()
+	{
+
 		//mesh.normals = new Vector3[]
 		//{
 		//	Vector3.up,
@@ -20,11 +23,11 @@ public class ProceduralMesh : MonoBehaviour
 		//	Vector3.up,
 		//	Vector3.up
 		//};
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 		//Vector3[] verts = mesh.vertices;
 
 		//verts[1] = new Vector3(0.5f, Time.realtimeSinceStartup, 0);
@@ -32,37 +35,89 @@ public class ProceduralMesh : MonoBehaviour
 		//mesh.vertices = verts;
 		//mesh.RecalculateBounds();
 		
-    }
+	}
 
 	public void GenerateMesh()
 	{
 		mesh = GetComponent<MeshFilter>().sharedMesh;
 
+		mesh = new Mesh();
 
-		mesh.vertices = new Vector3[]
+		VertexList.Clear();
+		VertexPointer.Clear();
+
+		VertexList.Add(new Vector3(0, 0, 0));
+		VertexPointer.Add(new Vector3(0, 0, 0), 0);
+
+		VertexList.Add(new Vector3(1, 0, 0));
+		VertexPointer.Add(new Vector3(1, 0, 0), 1);
+
+		VertexList.Add(new Vector3(1, 0, 1));
+		VertexPointer.Add(new Vector3(1, 0, 1), 2);
+
+		VertexList.Add(new Vector3(0, 0, 1));
+		VertexPointer.Add(new Vector3(0, 0, 1), 3);
+
+		VertexList.Add(new Vector3(2, 0, 0));
+		VertexPointer.Add(new Vector3(2, 0, 0), 4);
+
+		VertexList.Add(new Vector3(2, 0, 1));
+		VertexPointer.Add(new Vector3(2, 0, 1), 5);
+
+		Edge sideEdge = new Edge(new Vector3(1, 0, 0), new Vector3(1, 0, 1));
+
+		Quad quad = new Quad(new Edge[]
 		{
-			new Vector3(0,0,0),
-			new Vector3(0.5f,0,0),
-			new Vector3(1,0,0),
-			new Vector3(0.5f,0,0.5f),
-			new Vector3(0.5f,0,-0.5f),
-			new Vector3(1.5f,0,0.5f),
-			new Vector3(1.5f,0,-0.5f),
-			new Vector3(2, 0, 0)
-		};
+			new Edge(new Vector3(0,0,0), new Vector3(1,0,0)),
+			sideEdge,
+			new Edge(new Vector3(1,0,1), new Vector3(0,0,1)),
+			new Edge(new Vector3(0,0,1), new Vector3(0,0,0))
+		});
 
-		mesh.triangles = new int[]
+		Quad quad1 = new Quad(new Edge[]
 		{
-			0,3,1,
-			1,3,2,
-			0,2,4,
-			//1,2,4,
-			3,5,2,
-			2,6,4,
-			2,7,6,
-			2,5,7
-		};
+			new Edge(new Vector3(1,0,0), new Vector3(2,0,0)),
+			new Edge(new Vector3(2,0,0), new Vector3(2,0,1)),
+			new Edge(new Vector3(2,0,1), new Vector3(1,0,1)),
+			sideEdge
+		});
 
-		mesh.RecalculateNormals();
+		quad.SubDevide();
+		//quad.subQuads[0].SubDevide();
+
+		List<int> triList = new List<int>();
+
+		triList.AddRange(quad.GenerateTris());
+		triList.AddRange(quad1.GenerateTris());
+
+		mesh.vertices = VertexList.ToArray();
+		mesh.triangles = triList.ToArray();
+
+
+		//mesh.vertices = new Vector3[]
+		//{
+		//	new Vector3(0,0,0),
+		//	new Vector3(0.5f,0,0),
+		//	new Vector3(1,0,0),
+		//	new Vector3(0.5f,0,0.5f),
+		//	new Vector3(0.5f,0,-0.5f),
+		//	new Vector3(1.5f,0,0.5f),
+		//	new Vector3(1.5f,0,-0.5f),
+		//	new Vector3(2, 0, 0)
+		//};
+
+		//mesh.triangles = new int[]
+		//{
+		//	0,3,1,
+		//	1,3,2,
+		//	0,2,4,
+		//	//1,2,4,
+		//	3,5,2,
+		//	2,6,4,
+		//	2,7,6,
+		//	2,5,7
+		//};
+
+		//mesh.RecalculateNormals();
 	}
 }
---
+++ Assets/Scripts/Quad.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quad
+{
+	Vector3 mainVertex;
+	Edge[] edges;
+	public Quad[] subQuads;
+
+	public void SubDevide()
+	{
+		foreach (Edge edge in edges)
+		{
+			edge.SubDevide();
+		}
+
+		subQuads = new Quad[4];
+
+		subQuads[0] = new Quad(new Edge[]
+		{
+			edges[0].subEdges[0],
+			new Edge(edges[0].subEdges[0].vertexB, mainVertex),
+			new Edge(mainVertex, edges[3].subEdges[1].vertexA),
+			edges[3].subEdges[1]
+		});
+
+		subQuads[1] = new Quad(new Edge[]
+		{
+			edges[0].subEdges[1],
+			edges[1].subEdges[0],
+			new Edge(edges[1].subEdges[0].vertexB, mainVertex),
+			new Edge(mainVertex, edges[0].subEdges[0].vertexB)
+		});
+
+		subQuads[2] = new Quad(new Edge[]
+		{
+			edges[1].subEdges[1],
+			edges[2].subEdges[0],
+			new Edge(edges[2].subEdges[0].vertexB, mainVertex),
+			new Edge(mainVertex, edges[1].subEdges[0].vertexB)
+		});
+
+		subQuads[3] = new Quad(new Edge[]
+		{
+			edges[2].subEdges[1],
+			edges[3].subEdges[0],
+			new Edge(edges[3].subEdges[0].vertexB, mainVertex),
+			new Edge(mainVertex, edges[2].subEdges[0].vertexB)
+		});
+	}
+
+	public int[] GenerateTris()
+	{
+		List<int> triList = new List<int>();
+
+		if(subQuads.Length > 0)
+		{
+			for (int i = 0; i < subQuads.Length; i++)
+			{
+				triList.AddRange(subQuads[i].GenerateTris());
+			}
+		}
+		else
+		{
+			for (int i = 0; i < edges.Length; i++)
+			{
+				triList.AddRange(GenerateTriFromEdge(edges[i]));
+			}
+		}
+
+		return triList.ToArray();
+	}
+
+	int[] GenerateTriFromEdge(Edge edge)
+	{
+		List<int> triList = new List<int>();
+		if (edge.subEdges.Count > 0)
+		{
+			for (int i = 0; i < edge.subEdges.Count; i++)
+			{
+				triList.AddRange(GenerateTriFromEdge(edge.subEdges[i]));
+			}
+		}
+		else
+		{
+			triList.AddRange(edge.GenerateTriangle(mainVertex));
+		}
+		return triList.ToArray();
+	}
+
+
+	public Quad(Edge[] edges)
+	{
+		this.edges = edges;
+		subQuads = new Quad[0];
+
+		mainVertex = Vector3.Lerp(edges[0].vertexA, edges[2].vertexA, 0.5f);
+
+		ProceduralMesh.VertexList.Add(mainVertex);
+		ProceduralMesh.VertexPointer.Add(mainVertex, ProceduralMesh.VertexList.Count - 1);
+	}
+}
+
